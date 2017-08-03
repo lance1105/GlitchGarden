5bdc324bd34aabe73cceb191828ec040c7221a6b
Mark Wulff <mark.wulff@hotmail.com>
2017-08-03 14:31:34 +0200

    Initial commit, section 7 glitch garden

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public float autoLoadNextLevelAfter;
+
+    private void Start() {
+        Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+    }
+
+    // Load the game level
+    public void LoadLevel(string name) {
+        Application.LoadLevel(name);
+    }
+
+    // Quit the game
+    public void QuitRequest() {
+        Application.Quit();
+    }
+
+    // Load next level
+    public void LoadNextLevel() {
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
+}
