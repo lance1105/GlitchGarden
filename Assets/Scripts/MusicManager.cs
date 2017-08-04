4ee85d532dab8bed3c2f0115b806332a76dcba80
Mark Wulff <mark.wulff@hotmail.com>
2017-08-04 18:18:50 +0200

    Section 7, Not completed current lecture

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip[] levelMusicChangeArray;
+
+    private AudioSource audioSource;
+
+
+    void Awake() {
+        // Make this object persistent through every level, therefore do not
+        // destroy it when changing scenes.
+        DontDestroyOnLoad(this);
+
+    }
+
+    private void Start() {
+        // Get the audio source component from the object
+        audioSource = this.GetComponent<AudioSource>();
+    }
+
+    private void OnLevelWasLoaded(int level) {
+
+
+        AudioClip levelMusic = levelMusicChangeArray[level];
+
+        if (levelMusic) {
+            audioSource.clip = levelMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+
+
+    }
+
+
+
+}
